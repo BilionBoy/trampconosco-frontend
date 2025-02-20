a2d202d561c0c724dfa82455e5dc4e5b9c7feda3
BilionBoy <dhiogoandre26@gmail.com>
2025-02-20 17:05:17 -0400

    Simple Identação

--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -1,14 +1,7 @@
-import { useEffect } from 'react';
-import { StyleSheet } from 'react-native';
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  withTiming,
-  withRepeat,
-  withSequence,
-} from 'react-native-reanimated';
-
-import { ThemedText } from '@/components/ThemedText';
+import { useEffect }                                                                                        from 'react';
+import { StyleSheet }                                                                                       from 'react-native';
+import Animated, {useSharedValue,useAnimatedStyle,withTiming,withRepeat,withSequence,}                      from 'react-native-reanimated';
+import { ThemedText }                                                                                       from '@/components/ThemedText';
 
 export function HelloWave() {
   const rotationAnimation = useSharedValue(0);
